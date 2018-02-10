de2271f8af0a41432bd124647031f09573726f92
Ingrid Wong <yw340@georgetown.edu>
2018-02-10 04:13:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(num)
-    self.all.select do |student|
+    all_10th = self.all.select do |student|
       student.grade == "10"
     end
   end
